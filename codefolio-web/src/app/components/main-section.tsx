61a76fbf137bda9757a83594d6701e5e766a80b3
HarshK97 <harshkapse1234@gmail.com>
2025-10-09 13:53:42 +0530

    Migrated and Improved Skills and Project by adding more skills and projects and animations

--- codefolio-web/src/app/components/main-section.tsx
+++ codefolio-web/src/app/components/main-section.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import HeroSection from "@/app/components/hero-section";
 import AboutSection from "@/app/components/about-section";
+import ProjectSection from "@/app/components/project-section";
+import SkillsSection from "@/app/components/skills-section";
 
 const MainSection = () => {
     return (
         <>
             <HeroSection />
             <AboutSection />
+            <SkillsSection />
+            <ProjectSection />
         </>
     );
 };
